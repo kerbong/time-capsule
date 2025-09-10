ac32bf898515b16741bf9b3eff6c1137d2a586ce
kerbong <kerbong@gmail.com>
2025-09-10 22:43:45 +0900

    Updates

--- src/component/Capsule/CapsuleItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CapsuleItem = (props) => {
-  return <div>CapsuleItem</div>;
-};
-
-export default CapsuleItem;
--- src/component/RollingPaper/RollingPaper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RollingPaper = (props) => {
-  return <div>RollingPaper</div>;
-};
-
-export default RollingPaper;
